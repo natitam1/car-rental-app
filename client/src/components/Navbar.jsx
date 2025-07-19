2650e5d5eabcaf0a7d6bcf63422f6132d00db1ab
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-19 18:40:22 +0300

    Fix the bug that occured at the navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ setShowLogin }) => {
   const location = useLocation();
   const [open, setOpen] = useState(false);
   const navigate = useNavigate();
-  const [showLogin, setShowLogin] = useState(false);
+
   return (
     <div
       className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all  ${
