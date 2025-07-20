8183f2fa4a84e911fb655d669c8211ad371e90dc
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-20 09:12:04 +0300

    Add form to select location

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { assets } from "../assets/assets";
+import { assets, cityList } from "../assets/assets";
 
 const Hero = () => {
   return (
@@ -7,7 +7,21 @@ const Hero = () => {
       <h1 className="text-4xl font-semibold md:text-5xl">
         Luxury cars on Rent
       </h1>
-      <form></form>
+      <form className="flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]">
+        <div className="flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8">
+          <div>
+            <select required>
+              <option value="">Pickup Location</option>
+              {cityList.map((city) => (
+                <option key={city} value={city}>
+                  {city}
+                </option>
+              ))}
+            </select>
+            <p className="px-1 text-sm text-gray-500">Please select location</p>
+          </div>
+        </div>
+      </form>
       <img src={assets.main_car} alt="car" className="max-h-74" />
     </div>
   );
