0734d4a361f6db75cd3bd7a8cefc40bff6f574cc
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-24 14:25:07 +0300

    Finish the futured section

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -15,18 +15,18 @@ const FeaturedSection = () => {
         />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-18">
-        {dummyCarData.slice(0, 6).map((car) => {
+        {dummyCarData.slice(0, 6).map((car) => (
           <div key={car._id}>
             <CarCard car={car} />
-          </div>;
-        })}
+          </div>
+        ))}
       </div>
       <button
         onClick={() => {
           navigate("/cars");
           scrollTo(0, 0);
         }}
-        className="flex items-center justify-center gap-2 px-6 py-2 border-borderColor hover:bg-gray-50 rounded-md mt-18 cursor-pointer"
+        className="flex items-center justify-center gap-2 px-6 py-2 border border-borderColor hover:bg-gray-50 rounded-md mt-18 cursor-pointer"
       >
         Explore all cars <img src={assets.arrow_icon} alt="arrow" />
       </button>
