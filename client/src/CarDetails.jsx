8c0fa47d85c0468d527283de65482134eb00dac8
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-31 13:18:26 +0300

    Add a loader component

--- client/src/CarDetails.jsx
+++ client/src/CarDetails.jsx
@@ -29,6 +29,63 @@ const CarDetails = () => {
             alt=""
             className="w-full h-auto md:max-h-100 object-cover rounded-xl mb-6 shadow-md"
           />
+          <div className="space-y-6">
+            <div>
+              <h1 className="text-3xl font-bold">
+                {car.brand} {car.model}
+              </h1>
+              <p className="text-gray-500 text-lg">
+                {car.category} • {car.year}
+              </p>
+            </div>
+            <hr className="border-borderColor my-6" />
+            <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+              {[
+                {
+                  icon: assets.users_icon,
+                  text: `${car.seating_capacity} Seats`,
+                },
+                {
+                  icon: assets.fuel_icon,
+                  text: `${car.transmission}`,
+                },
+                {
+                  icon: assets.location_icon,
+                  text: `${car.location}`,
+                },
+              ].map(({ icon, text }) => (
+                <div
+                  key={text}
+                  className="flex flex-col items-center bg-light p-4 rounded-lg"
+                >
+                  <img src={icon} alt="" className="h-5 mb-2" />
+                  {text}
+                </div>
+              ))}
+            </div>
+            {/* Description  */}
+            <div>
+              <h1 className="text-xl font-medium mb-3">Description</h1>
+              <p className="text-gray-500">{car.description}</p>
+            </div>
+            {/* Features  */}
+            <div>
+              <h1 className="text-xl font-medium mb-3">Features</h1>
+              <ul className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+                {[
+                  "360 Camera",
+                  "Bluetooth",
+                  "GPS",
+                  "Heated Seats",
+                  "Rear View Mirror",
+                ].map((item) => (
+                  <li key={item} className="flex items-center text-gray-500">
+                    <img src={assets.check_icon} className="h-4 mr-2" alt="" />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
