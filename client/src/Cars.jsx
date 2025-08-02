d0f531e1bbefd33e6bdd1851bf86f541beeba195
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-02 10:41:35 +0300

    Complete the cars list page

--- client/src/Cars.jsx
+++ client/src/Cars.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Title from "./components/Title";
-import { assets } from "./assets/assets";
+import { assets, dummyCarData } from "./assets/assets";
+import CarCard from "./components/CarCard";
 
 const Cars = () => {
   const [input, serInput] = useState("");
@@ -21,6 +22,20 @@ const Cars = () => {
             className="w-full h-full outline-none text-gray-500"
           />
           <img src={assets.filter_icon} className="w-4.5 h-4.5 ml-2" alt="" />
+        </div>
+      </div>
+      <div>
+        <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-10">
+          <p className="text-gray-500 xl:px-20 max-w-7xl mx-auto">
+            {dummyCarData.length} Cars
+          </p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-4 xl:px-20 max-w-7xl mx-auto">
+            {dummyCarData.map((car, index) => (
+              <div key={index}>
+                <CarCard car={car} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
