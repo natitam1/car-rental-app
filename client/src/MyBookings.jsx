5085c267a217ace4bf44e291f2f196e55d881408
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-04 12:15:20 +0300

    Fetch cummmy my booking data

--- client/src/MyBookings.jsx
+++ client/src/MyBookings.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dummyMyBookingsData } from "./assets/assets";
 
 const MyBookings = () => {
-  return <div>MyBookings</div>;
+  const [bookings, setBookings] = useState([]);
+
+  const fetchMyBookings = async () => {
+    setBookings(dummyMyBookingsData);
+  };
+
+  useEffect(() => {
+    fetchMyBookings();
+  }, []);
+  return <div></div>;
 };
 
 export default MyBookings;
