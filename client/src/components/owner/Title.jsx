e5caf9284ef7de21d49a7f68252ad6c95df97145
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-05 11:43:14 +0300

    Create a title component

--- client/src/components/owner/Title.jsx
+++ client/src/components/owner/Title.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Title = () => {
-  return <div>Title</div>;
+const Title = ({ title, subTitle }) => {
+  return (
+    <>
+      <h1 className="font-medium text-3xl">{title}</h1>
+      <p className="text-sm md:text-base text-gray-500/90 mt-2">{subTitle}</p>
+    </>
+  );
 };
 
 export default Title;
