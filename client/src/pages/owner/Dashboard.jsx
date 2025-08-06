d79202406976747fccf955b0c2bd53b3023752b9
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-06 08:57:48 +0300

    Add a title commponent to the admin dashboard

--- client/src/pages/owner/Dashboard.jsx
+++ client/src/pages/owner/Dashboard.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { assets, dummyDashboardData } from "../../assets/assets";
+import Title from "../../components/owner/Title";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [data, setData] = useState({
+    totalCars: 0,
+    totalBookings: 0,
+    pendingBookings: 0,
+    completedBookings: 0,
+    recentBookings: [],
+    monthlyRevenue: 0,
+  });
+
+  const dashboardCards = [
+    { title: "Total Cars", value: data.totalCars, icon: assets.carIconColored },
+    {
+      title: "Total Bookings",
+      value: data.totalBookings,
+      icon: assets.listIconColored,
+    },
+    {
+      title: "Pending",
+      value: data.pendingBookings,
+      icon: assets.cautionIconColored,
+    },
+    {
+      title: "Confirmed",
+      value: data.completedBookings,
+      icon: assets.listIconColored,
+    },
+  ];
+
+  useEffect(() => {
+    setData(dummyDashboardData);
+  }, []);
+
+  return (
+    <div className="px-4 pt-10 md:px-10 flex-1">
+      <Title
+        title="Admin Dashboard"
+        subTitle="Monitor overall platform performance including total cars, bookings, and recent activities"
+      />
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-8 max-w-3xl">
+        {dashboardCards.map((card, index) => (
+          <div></div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
