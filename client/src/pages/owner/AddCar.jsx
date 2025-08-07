0c5c5268f59972ce182426b265fc93fa261e0956
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-07 08:04:42 +0300

    Add image and car state variables

--- client/src/pages/owner/AddCar.jsx
+++ client/src/pages/owner/AddCar.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddCar = () => {
+  const [image, setImage] = useState(null);
+  const [car, setCar] = useState({
+    brand: "",
+    model: "",
+    year: 0,
+    pricePerDay: 0,
+    category: "",
+    transmission: "",
+    fuel_type: "",
+    seating_capacity: 0,
+    location: "",
+    description: "",
+  });
   return <div>AddCar</div>;
 };
 
