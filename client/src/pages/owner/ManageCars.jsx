0633834158fd42a88d0d21982d65c66714876373
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-08 09:14:24 +0300

    Set up the manage car component

--- client/src/pages/owner/ManageCars.jsx
+++ client/src/pages/owner/ManageCars.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dummyCarData } from "../../assets/assets";
+import Title from "../../components/owner/Title";
 
 const ManageCars = () => {
-  return <div>ManageCars</div>;
+  const [cars, setCars] = useState([]);
+
+  const fetchOwnerCars = async () => {
+    setCars(dummyCarData);
+  };
+  useEffect(() => {
+    fetchOwnerCars();
+  }, []);
+
+  return (
+    <div className="px-4 pt-10 md:px-10 w-full">
+      <Title
+        title="Manage Cars"
+        subTitle="View all listed cars, update their details, or remove them from the booking platform."
+      />
+      <div className="max-w-3xl w-full rounded-md overflow-hidden border border-borderColor mt-6"></div>
+    </div>
+  );
 };
 
 export default ManageCars;
